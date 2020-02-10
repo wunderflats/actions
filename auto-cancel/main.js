24b09c310095a6180d87a8ec98028c56d6477a92
Emmanuel Viala <emmanuel.viala@wunderflats.com>
2020-02-10 15:12:59 +0100

    Fix action for pull_request

--- auto-cancel/main.js
+++ auto-cancel/main.js
@@ -1,9 +1,19 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 
+const actionEvents = require(process.env.GITHUB_EVENT_PATH);
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/", 2);
-const commit_hash = process.env.GITHUB_SHA;
-const branch = process.env.GITHUB_REF.replace("refs/heads/", "");
+const branch = process.env.GITHUB_HEAD_REF
+  ? process.env.GITHUB_HEAD_REF
+  : process.env.GITHUB_REF.replace("refs/heads/", "");
+
+// In case you are wondering, github is not consistent here. On the UI it will give you
+// the commit hash of your branch head, but in reality GITHUB_SHA is the commit hash
+// of your commit on the target branch of your PR. So we parse it from the event file.
+const commit_hash = process.env.GITHUB_HEAD_REF
+  ? actionEvents.pull_request.head.sha
+  : process.env.GITHUB_SHA;
+
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 
 async function run() {
