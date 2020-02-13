c1a627e0ca65349c60c3047756fce36dd60ef8a9
Njibhu <emmanuel.viala@wunderflats.com>
2020-02-13 13:18:19 +0100

    First version of reviewer picker (#1)
    Adding reviewer picker

---
+++ random-reviewer-picker/main.js
@@ -0,0 +1,140 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+const { random } = require("lodash");
+
+/**
+ * Fetch and transform inputs into usable variables
+ **/
+function getInputs() {
+  const reviewerList = core
+    .getInput("reviewer_list")
+    .split(",")
+    .map(i => i.replace(" ", ""));
+  const reviewerAmount = parseInt(core.getInput("reviewer_amount"));
+  const maintainerList = core
+    .getInput("maintainer_list")
+    .split(",")
+    .map(i => i.replace(" ", ""));
+  const maintainerAmount = parseInt(core.getInput("maintainer_amount"));
+  const skipBusy = !!core.getInput("skip_busy");
+
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+  return {
+    reviewerList,
+    reviewerAmount,
+    maintainerList,
+    maintainerAmount,
+    skipBusy,
+    GITHUB_TOKEN
+  };
+}
+
+/**
+ * Get user "busy" status from github API
+ **/
+async function isUserBusy(octokit, userHandle) {
+  core.info(`Checking status of "${userHandle}"`);
+
+  const { user } = await octokit.graphql({
+    query: `
+    query userStatus($user: String!) {
+      user(login: $user) {
+        status {
+          indicatesLimitedAvailability
+        }
+      }
+    }`,
+    user: userHandle
+  });
+
+  // If user never changed their status, then status is null
+  if (user.status && user.status.indicatesLimitedAvailability) {
+    return userStatus.user.status.indicatesLimitedAvailability;
+  }
+
+  return false;
+}
+
+/**
+ * Remove busy user if needed from an array of user names
+ **/
+async function pickUsers(userList, pickAmount, removeBusy) {
+  const pickedUsers = [];
+  const filteredList = [];
+
+  if (removeBusy) {
+    for (const user of userList) {
+      if (!(await isUserBusy(octokit, user))) {
+        filteredList.push(user);
+      }
+    }
+  } else {
+    filteredList = userList;
+  }
+
+  for (let i = 0; i < pickAmount; i++) {
+    pickedUsers.push(filteredList[random(0, filteredList.length - 1)]);
+  }
+
+  return pickedUsers;
+}
+
+/**
+ * Main action entry point
+ **/
+async function run() {
+  //Get action events
+  const actionEvents = require(process.env.GITHUB_EVENT_PATH);
+
+  const {
+    reviewerList,
+    reviewerAmount,
+    maintainerList,
+    maintainerAmount,
+    skipBusy,
+    GITHUB_TOKEN
+  } = getInputs();
+
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/", 2);
+  const prNumber = actionEvents.pull_request.number;
+  core.info(`Adding reviewers to pull request #${prNumber}`);
+
+  const octokit = new github.GitHub(GITHUB_TOKEN);
+
+  // Get current reviewers info
+  const { users, teams } = (
+    await octokit.pulls.listReviewRequests({
+      owner,
+      repo,
+      pull_number: prNumber
+    })
+  ).data;
+
+  // If we needs to add reviewers
+  if (users.length === 0 && teams.length === 0) {
+    core.info("Reviewers needed, picking reviewers.");
+
+    // Reviewers:
+    const reviewers = await pickUsers(reviewerList, reviewerAmount, skipBusy);
+
+    // Maintainers:
+    const maintainers = await pickUsers(
+      maintainerList,
+      maintainerAmount,
+      skipBusy
+    );
+
+    core.info(
+      `Assigning ${[...reviewers, ...maintainers].join(", ")} to code review`
+    );
+    await octokit.pulls.createReviewRequest({
+      owner,
+      repo,
+      pull_number: prNumber,
+      reviewers: [...reviewers, ...maintainers]
+    });
+  }
+}
+
+run().catch(error => core.setFailed(error.message));
