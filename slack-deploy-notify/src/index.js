0e2406b3c4e136e4e3a604b55d03ecc95806d63d
Emmanuel Viala <emmanuel.viala@wunderflats.com>
2020-03-14 16:49:56 +0100

    Add crossmark

--- slack-deploy-notify/src/index.js
+++ slack-deploy-notify/src/index.js
@@ -22,11 +22,10 @@ const commit =
 
 const eventMap = {
   DEPLOYMENT_TEST_FAIL: {
-    text: `A test check failed preventing deployment on master
-    ${commit}<${runLink}|See github action>`
+    text: `❌ A test check failed preventing deployment on master ${commit}<${runLink}|See github action>`
   },
   DEPLOYMENT_PAUSED: {
-    text: `The deployment for master ${commit}has been paused because of another running deployment. Please resume it when the first deployment is green.
+    text: `❌ The deployment for master ${commit}has been paused because of another running deployment. Please resume it when the first deployment is green.
 <${runLink}|See github action> \n
 <${masterActionPage}|See all running actions>`
   }
