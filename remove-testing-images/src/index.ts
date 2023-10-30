12f6cb239cab2d3ee75d79923d5964593338b68c
Samira Moghadam <85633118+ronnieFrown92@users.noreply.github.com>
2023-10-30 13:46:38 +0100

    Add `remove-testing-image` action (#70)
    * add the new action
    
    * allow deletion
    
    * some improvements
    
    * remove the test log

---
+++ remove-testing-images/src/index.ts
@@ -0,0 +1,131 @@
+import * as github from "@actions/github";
+import * as core from "@actions/core";
+
+const token = core.getInput("GITHUB_TOKEN", { required: true });
+const packageName = core.getInput("PACKAGE_NAME", { required: true });
+const tag = core.getInput("TAG");
+
+const octokit = github.getOctokit(token);
+
+await run();
+
+async function run() {
+  const acceptedPackageNames = [
+    "api-testing",
+    "website-testing",
+    "crm-testing",
+    "lld-testing",
+  ];
+
+  if (!acceptedPackageNames.includes(packageName)) {
+    core.setFailed(`package name ${packageName} is not supported.`);
+    return;
+  }
+
+  try {
+    if (tag) {
+      const testingImageId = await findTestingImageByTag();
+
+      if (!testingImageId) {
+        throw new Error(`No testing image found with tag ${tag}`);
+      }
+
+      return removeTestingImage(testingImageId);
+    }
+
+    return cleanupUnneededTestingImages();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function findTestingImageByTag() {
+  core.info(`🔦 looking for testing image with tag ${tag}...`);
+
+  let imagesWithRequestedTag: number | undefined;
+
+  await octokit.paginate(
+    octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
+    {
+      package_type: "container",
+      state: "active",
+      package_name: packageName,
+      org: "wunderflats",
+      per_page: 100,
+    },
+    (response, done) => {
+      const testingImages = response.data;
+      const targetImage = testingImages.find((v) =>
+        v.metadata.container.tags.includes(tag)
+      );
+
+      if (targetImage) {
+        imagesWithRequestedTag = targetImage.id;
+        done();
+      }
+
+      return response.data;
+    }
+  );
+
+  return imagesWithRequestedTag;
+}
+
+async function cleanupUnneededTestingImages() {
+  core.info("🔦 finding the unneeded images to cleanup...");
+  const imagesToBeDeleted: number[] = [];
+
+  for await (const response of octokit.paginate.iterator(
+    octokit.rest.packages.getAllPackageVersionsForPackageOwnedByOrg,
+    {
+      package_type: "container",
+      package_name: packageName,
+      org: "wunderflats",
+      state: "active",
+      per_page: 100,
+    }
+  )) {
+    const testingImages = response.data;
+    for (const image of testingImages) {
+      const imageTags = image.metadata.container.tags;
+
+      const isUnnecessaryImageWithHashTag =
+        imageTags.length === 1 &&
+        imageTags[0].length === 40 &&
+        /^[A-F0-9]+$/i.test(imageTags[0]); // Hexadecimal check
+
+      if (imageTags.length == 0 || isUnnecessaryImageWithHashTag) {
+        imagesToBeDeleted.push(image.id);
+      }
+    }
+  }
+
+  if (imagesToBeDeleted.length > 0) {
+    core.startGroup(
+      `🧹 starting the cleanup of ${imagesToBeDeleted.length} testing images`
+    );
+
+    for (const imageId of imagesToBeDeleted) {
+      await removeTestingImage(imageId);
+    }
+
+    core.endGroup();
+  }
+}
+
+async function removeTestingImage(imageId) {
+  core.info(`⏳ image with id ${imageId} is about to be deleted...`);
+
+  try {
+    await octokit.rest.packages.deletePackageVersionForOrg({
+      package_type: "container",
+      package_name: packageName,
+      org: "wunderflats",
+      package_version_id: imageId,
+    });
+
+    core.info(`✅ image with id ${imageId} deleted.`);
+  } catch (error) {
+    throw new Error(`package #${imageId} not deleted: ${error.message}`);
+  }
+}
