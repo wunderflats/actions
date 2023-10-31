37131307603927d8c5e47581d56de29963f4ee5f
ronnieFrown92 <samira.saremi@gmail.com>
2023-10-31 13:05:29 +0100

    comment the removal for test

--- remove-testing-images/src/index.ts
+++ remove-testing-images/src/index.ts
@@ -138,12 +138,12 @@ async function removeTestingImage(imageId) {
   core.info(`⏳ image with id ${imageId} is about to be deleted...`);
 
   try {
-    await octokit.rest.packages.deletePackageVersionForOrg({
-      package_type: "container",
-      package_name: packageName,
-      org: "wunderflats",
-      package_version_id: imageId,
-    });
+    // await octokit.rest.packages.deletePackageVersionForOrg({
+    //   package_type: "container",
+    //   package_name: packageName,
+    //   org: "wunderflats",
+    //   package_version_id: imageId,
+    // });
 
     core.info(`✅ image with id ${imageId} deleted.`);
   } catch (error) {
