51c9c69ddfff4823fc038ebf3e53c95df405e89c
Sergei Kovalenko <sergei.kovalenko@wunderflats.com>
2024-02-21 16:01:42 +0100

    Extent `upload-to-test-dashboard` action to provide runAttempt from github context

---
+++ upload-to-test-dashboard/src/config.ts
@@ -0,0 +1,15 @@
+import * as env from "env-var";
+import github from "@actions/github";
+
+export const config = {
+  apiUrl: env.get("INPUT_API_URL").required().asString(),
+  repository: env.get("INPUT_REPOSITORY").required().asString(),
+  branch: env.get("INPUT_BRANCH").required().asString(),
+  testSuite: env.get("INPUT_TEST_SUITE").required().asString(),
+  testFileType: env.get("INPUT_TEST_FILE_TYPE").required().asString(),
+  commitHash: env.get("INPUT_COMMIT_HASH").required().asString(),
+  jobId: env.get("INPUT_JOB_ID").required().asString(),
+  files: env.get("INPUT_FILES").required().asString(),
+  runAttempt: github.context.runNumber.toString(10),
+  pushToken: env.get("INPUT_DASHBOARD_PUSH_TOKEN").required().asString(),
+};
